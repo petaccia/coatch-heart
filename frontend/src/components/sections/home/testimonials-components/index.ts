d8b0d767dc5fb4542aeb07040a15ff5d638d2511
petaccia <100707989+petaccia@users.noreply.github.com>
2025-04-17 21:47:56 +0200

    refactor: divide TestimonialsSection component into smaller reusable components

---
+++ frontend/src/components/sections/home/testimonials-components/index.ts
@@ -0,0 +1,4 @@
+export { default as SectionTitle } from './SectionTitle';
+export { default as TestimonialCard } from './TestimonialCard';
+export { default as NavigationButtons } from './NavigationButtons';
+export { default as NavigationDots } from './NavigationDots';
