6f029fb697a23fa678004b40420a971588af833c
petaccia <100707989+petaccia@users.noreply.github.com>
2025-04-18 09:36:25 +0200

    feat: add images for partners and blog sections

--- frontend/src/data/partnersData.ts
+++ frontend/src/data/partnersData.ts
@@ -10,42 +10,42 @@ export const partnersData: Partner[] = [
   {
     id: 1,
     name: "Fédération Française de Football",
-    logo: "/images/partners/fff-logo.svg",
+    logo: "/images/partners/fff.svg",
     width: 120,
     height: 60
   },
   {
     id: 2,
     name: "Ligue 1",
-    logo: "/images/partners/ligue1-logo.svg",
-    width: 100,
+    logo: "/images/partners/ligue1.svg",
+    width: 60,
     height: 60
   },
   {
     id: 3,
     name: "UEFA",
-    logo: "/images/partners/uefa-logo.svg",
-    width: 100,
+    logo: "/images/partners/uefa.svg",
+    width: 60,
     height: 60
   },
   {
     id: 4,
     name: "FIFA",
-    logo: "/images/partners/fifa-logo.svg",
-    width: 100,
+    logo: "/images/partners/fifa.svg",
+    width: 60,
     height: 60
   },
   {
     id: 5,
     name: "Paris Saint-Germain",
-    logo: "/images/partners/psg-logo.svg",
+    logo: "/images/partners/psg.svg",
     width: 60,
     height: 60
   },
   {
     id: 6,
     name: "Olympique de Marseille",
-    logo: "/images/partners/om-logo.svg",
+    logo: "/images/partners/om.svg",
     width: 60,
     height: 60
   }
